97105eadeaee2c1f528f5bfae7ae0af7909316d1
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-05 11:17:32 +0530

    Added a fallback news page for missing newsId

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Page() {
   return (
-    <div>Page</div>
+    <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
+      <h1 className='text-center text-3xl'>Oops! Your URL is missing a News ID.</h1>
+      <p className='text-center text-xl '>Browse back to News Page.{" "}</p>
+      <Link href={'/news-and-updates'} className='text-xl text-sky-400 underline'>
+          Click Here
+      </Link>
+    </div>
   )
 }
 

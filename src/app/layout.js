a6dc4f3e06648fab20f719d08f7f11530f82195e
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-06 18:39:54 +0530

    Minor Changes in Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
-'use client'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Manrope } from 'next/font/google';
-import { Providers } from '../Providers/Provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +10,14 @@ const manrope = Manrope({
   variable: "--font-manrope",
 });
 
+export const metadata = {
+  title: 'AD Network',
+  description: 'Generated with passion by Meraki Studio',
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${manrope.className} !bg-[#00171F] bg-no-repeat duration-300 text-[#fff]`}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className={`${manrope.className} !bg-[#00171F] bg-no-repeat duration-300 text-[#fff]`}>{children}</body>
     </html>
   )
 }
